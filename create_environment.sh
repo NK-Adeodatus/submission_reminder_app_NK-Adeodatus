73b023c8e45d276fbd41163474704717d08fdd40
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-10-03 10:45:17 -0700

    individual project complete

---
+++ create_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir submission_reminder_app
+mkdir submission_reminder_app/app
+mkdir submission_reminder_app/modules
+mkdir submission_reminder_app/assets
+mkdir submission_reminder_app/config
+
+#creating the files
+touch submission_reminder_app/app/reminder.sh
+touch submission_reminder_app/modules/functions.sh
+touch submission_reminder_app/assets/submissions.sh
+touch submission_reminder_app/config/config.env
+touch submission_reminder_app/startup.sh
